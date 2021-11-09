cd6fc36093c707601a4543a1688a8d8f569f9c1c
Their Name <lars.branzell@protonmail.com>
2021-11-09 16:59:30 +0100

    logos

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-var express = require('express');
-var passport = require('passport');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var passport = require("passport");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var myaccountRouter = require('./routes/myaccount');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
+var adminRouter = require("./routes/admin");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-require('./boot/db')();
-require('./boot/auth')();
+require("./boot/db")();
+require("./boot/auth")();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(function(req, res, next) {
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(function (req, res, next) {
   var msgs = req.session.messages || [];
   res.locals.messages = msgs;
-  res.locals.hasMessages = !! msgs.length;
+  res.locals.hasMessages = !!msgs.length;
   req.session.messages = [];
   next();
 });
 app.use(passport.initialize());
-app.use(passport.authenticate('session'));
+app.use(passport.authenticate("session"));
 
 // Define routes.
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/myaccount', myaccountRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/admin", adminRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { user: req.user });
+router.get("/", function (req, res, next) {
+  res.render("index", { user: req.user });
 });
 
 module.exports = router;
